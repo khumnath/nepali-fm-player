8485cabdd8d10cf934654e1453b9aa1d0ffec788
khumnath <nath.khum@gmail.com>
2024-07-19 13:54:15 +0900

    changed design and added new stations
    replaced icon font with font awesome

--- js/manager.js
+++ js/manager.js
@@ -1,9 +1,7 @@
 (function (w) {
     function Controls(config) {
         this.lastVol = 1;
-        this.listShowFlag = 0;
         this.songIndex = 0;
-        this.timer = null;
         this.modeFlag = 0;
         if (config) {
             this.init(config);
@@ -12,7 +10,7 @@
 
     Controls.prototype = {
         constructor: Controls,
-        init(config){
+        init(config) {
             this.music = document.querySelector('.music');
             this.plyBtn = document.querySelector('.ply-btn');
             this.pIcon = this.plyBtn.getElementsByTagName('i')[0];
@@ -28,7 +26,6 @@
             this.songName = document.querySelector('.name');
             this.author = document.querySelector('.singer');
             this.songContainer = document.querySelector('.songContainer');
-            this.menu = document.querySelector('.menu');
             this.preSongBtn = document.querySelector('.preSongBtn');
             this.nextSongBtn = document.querySelector('.nextSongBtn');
             this.playMode = document.querySelector('.playMode');
@@ -39,40 +36,52 @@
             this.listCircle();
             this.addEvent();
             this.updateSong(0);
-            this.music.play().then(() => {
-                this.music.muted = false;
-            }).catch((error) => {
-                console.log('Autoplay failed', error);
-                this.music.play();
-            }); // Add autoplay with error handling
+            this.autoPlay();
         },
-        updateSong(index){
+        updateSong(index) {
             this.songIndex = index;
             const song = this.songs[this.songIndex];
             this.music.src = song.src;
             this.cover.src = song.cover;
             this.songName.innerHTML = song.name;
             this.author.innerHTML = song.author;
+            this.Ttime.innerHTML = '0.00'; // Reset Ttime to default value initially
         },
         initialList() {
             const list = document.createElement('ul');
             this.songs.forEach((song, index) => {
                 const item = document.createElement('li');
-                item.innerHTML = song.name;
+                const statusDot = document.createElement('span');
+                statusDot.classList.add('status-dot');
+                item.appendChild(statusDot);
+                item.append(song.name);
                 item.addEventListener('click', () => {
                     this.updateSong(index);
-                    this.music.play(); // Add autoplay on selection
+                    this.music.play();
                 });
                 list.appendChild(item);
+                this.checkStreamStatus(song.src, statusDot);
             });
             this.songContainer.appendChild(list);
+        },
+        checkStreamStatus(url, statusDot) {
+            const audio = new Audio();
+            audio.src = url;
+            audio.addEventListener('canplaythrough', () => {
+                statusDot.classList.add('online');
+            });
+            audio.addEventListener('error', () => {
+                statusDot.classList.add('offline');
+            });
+            // Attempt to load the stream
+            audio.load();
         },
         hideControls() {
             this.music.controls = false;
         },
         listCircle() {
             this.modeFlag = 0;
-            this.modeIcon.className = 'iconfont icon-list-cycle';
+            this.modeIcon.className = 'fas fa-list';
         },
         addEvent() {
             this.plyBtn.addEventListener('click', () => {
@@ -83,38 +92,39 @@
                 }
             });
             this.music.addEventListener('play', () => {
-                this.pIcon.className = 'iconfont icon-pause';
+                this.pIcon.className = 'fas fa-pause';
             });
             this.music.addEventListener('pause', () => {
-                this.pIcon.className = 'iconfont icon-play';
+                this.pIcon.className = 'fas fa-play';
             });
             this.music.addEventListener('timeupdate', () => {
                 this.Ctime.innerHTML = this.formatTime(this.music.currentTime);
                 this.progress.style.width = (this.music.currentTime / this.music.duration) * 100 + '%';
             });
+            this.music.addEventListener('loadedmetadata', () => {
+                if (isNaN(this.music.duration) || this.music.duration === Infinity) {
+                    this.Ttime.innerHTML = 'stream';
+                } else {
+                    this.Ttime.innerHTML = this.formatTime(this.music.duration);
+                }
+            });
             this.music.addEventListener('ended', () => {
                 this.nextSong();
             });
-            this.progressBar.addEventListener('click', (e) => {
-                const rate = e.offsetX / this.progressBar.offsetWidth;
-                this.music.currentTime = this.music.duration * rate;
-            });
+            this.progressBar.addEventListener('mousedown', this.startSeek.bind(this));
+            this.progressBar.addEventListener('touchstart', this.startSeek.bind(this), { passive: true });
+            this.vol.addEventListener('mousedown', this.startVolume.bind(this));
+            this.vol.addEventListener('touchstart', this.startVolume.bind(this), { passive: true });
             this.noVol.addEventListener('click', () => {
                 if (this.music.muted) {
                     this.music.muted = false;
-                    this.vIcon.className = 'iconfont icon-sound';
+                    this.vIcon.className = 'fas fa-volume-up';
                     this.nowVol.style.width = this.lastVol * 100 + '%';
                 } else {
                     this.music.muted = true;
-                    this.vIcon.className = 'iconfont icon-mute';
+                    this.vIcon.className = 'fas fa-volume-mute';
                     this.nowVol.style.width = 0;
                 }
-            });
-            this.vol.addEventListener('click', (e) => {
-                const rate = e.offsetX / this.vol.offsetWidth;
-                this.music.volume = rate;
-                this.lastVol = rate;
-                this.nowVol.style.width = rate * 100 + '%';
             });
             this.preSongBtn.addEventListener('click', () => {
                 this.preSong();
@@ -159,14 +169,79 @@
         },
         singleCycle() {
             this.modeFlag = 1;
-            this.modeIcon.className = 'iconfont icon-single-cycle';
+            this.modeIcon.className = 'fas fa-repeat';
             this.music.loop = true;
         },
         randomPlay() {
             this.modeFlag = 2;
-            this.modeIcon.className = 'iconfont icon-random-play';
+            this.modeIcon.className = 'fas fa-random';
             this.music.loop = false;
+        },
+        autoPlay() {
+            this.music.play().then(() => {
+                this.music.muted = false;
+            }).catch((error) => {
+                console.log('Autoplay failed');
+                this.music.play();
+            });
+        },
+        getVolumeIcon(volume) {
+            if (volume === 0) {
+                return 'fas fa-volume-mute';
+            } else if (volume <= 0.5) {
+                return 'fas fa-volume-down';
+            } else {
+                return 'fas fa-volume-up';
+            }
+        },
+        startSeek(e) {
+            this.isSeeking = true;
+            document.addEventListener('mousemove', this.seek.bind(this));
+            document.addEventListener('mouseup', this.stopSeek.bind(this));
+            document.addEventListener('touchmove', this.seek.bind(this), { passive: true });
+            document.addEventListener('touchend', this.stopSeek.bind(this));
+            this.seek(e);
+        },
+        seek(e) {
+            if (this.isSeeking) {
+                const clientX = e.clientX || e.touches[0].clientX;
+                const rate = (clientX - this.progressBar.getBoundingClientRect().left) / this.progressBar.offsetWidth;
+                this.music.currentTime = Math.max(0, Math.min(rate * this.music.duration, this.music.duration));
+            }
+        },
+        stopSeek() {
+            this.isSeeking = false;
+            document.removeEventListener('mousemove', this.seek.bind(this));
+            document.removeEventListener('mouseup', this.stopSeek.bind(this));
+            document.removeEventListener('touchmove', this.seek.bind(this));
+            document.removeEventListener('touchend', this.stopSeek.bind(this));
+        },
+        startVolume(e) {
+            this.isAdjustingVolume = true;
+            document.addEventListener('mousemove', this.adjustVolume.bind(this));
+            document.addEventListener('mouseup', this.stopVolume.bind(this));
+            document.addEventListener('touchmove', this.adjustVolume.bind(this), { passive: true });
+            document.addEventListener('touchend', this.stopVolume.bind(this));
+            this.adjustVolume(e);
+        },
+        adjustVolume(e) {
+            if (this.isAdjustingVolume) {
+                const clientX = e.clientX || e.touches[0].clientX;
+                const rate = (clientX - this.vol.getBoundingClientRect().left) / this.vol.offsetWidth;
+                this.music.volume = Math.max(0, Math.min(rate, 1));
+                this.lastVol = this.music.volume;
+                this.nowVol.style.width = this.music.volume * 100 + '%';
+                this.vIcon.className = this.getVolumeIcon(this.music.volume);
+            }
+        },
+        stopVolume() {
+            this.isAdjustingVolume = false;
+            document.removeEventListener('mousemove', this.adjustVolume.bind(this));
+            document.removeEventListener('mouseup', this.stopVolume.bind(this));
+            document.removeEventListener('touchmove', this.adjustVolume.bind(this));
+            document.removeEventListener('touchend', this.stopVolume.bind(this));
         }
     };
+
     w.Controls = Controls;
 })(window);
--- js/playlist.js
+++ js/playlist.js
@@ -1,48 +1,175 @@
-window.onload=function(){
-		new Controls({
-			songs:[ {
-                        src:'https://stream1.radionepal.gov.np/live/',
-                        name:'Radio Nepal',
-                        author:'am/sw/fm radio',
-                        cover:'img/radionepal.png'
-                    },        
-                    {
-                        src:'https://radio-broadcast.ekantipur.com/stream/',
-                        name:'Kantipur Fm',
-                        author:'91.2 MHZ kathmandu',
-                        cover:'https://radio-jcss-cdn.ekantipur.com/images/kfm_logo.png'
-                    },
-                    {
-                        src:'https://stream.zeno.fm/h527zwd11uquv',
-                        name:'ujyalo 90 network',
-                        author:'90 MHZ kathmandu',
-                        cover:'https://unncdn.prixacdn.net/static/frontend/img/logo.png'
-                    },
-                    {
-                        src:'http://kalika-stream.softnep.com:7740/;',
-                        name:'kalika fm',
-                        author:'95.2 MHZ butwal',
-                        cover:'img/kalikafm.png'
-                    },
-                    {
-                        src:'https://live.itech.host:9107/stream',
-                        name:'galyang fm',
-                        author:'92.2 MHZ syangja',
-                        cover:'img/galyangfm.png'
-                    },
-                    {
-                        src:'https://stream.zeno.fm/fvrx47wpg0quv',
-                        name:'radio audio',
-                        author:'106.3 MHZ kathmandu',
-                        cover:'img/radioaudio.png'
-                    },
-                    {
-                        src:'https://live.itech.host:9598/stream',
-                        name:'shreenagar fm',
-                        author:'93.2 MHZ tansen palpa',
-                        cover:'img/shreenagarfm.png'
-                    },
-                    
-                ]
-		});
-	}
+window.onload = function () {
+    new Controls({
+        songs: [
+            {
+                "src": "https://stream1.radionepal.gov.np/live/",
+                "name": "Radio Nepal",
+                "author": "am/sw/fm radio",
+                "cover": "img/radionepal.png"
+            },
+            {
+                "src": "https://radio-broadcast.ekantipur.com/stream/",
+                "name": "Kantipur Fm",
+                "author": "91.2 MHZ kathmandu",
+                "cover": "img/radio_kantipur.png"
+            },
+            {
+                "src": "https://stream.zeno.fm/h527zwd11uquv",
+                "name": "ujyalo 90 network",
+                "author": "90 MHZ kathmandu",
+                "cover": "img/ujyalo_90_network.png"
+            },
+            {
+                "src": "http://kalika-stream.softnep.com:7740/;",
+                "name": "kalika fm",
+                "author": "95.2 MHZ butwal",
+                "cover": "img/kalikafm.png"
+            },
+            {
+                "src": "https://live.itech.host:9107/stream",
+                "name": "galyang fm",
+                "author": "92.2 MHZ syangja",
+                "cover": "img/galyangfm.png"
+            },
+            {
+                "src": "https://stream.zeno.fm/fvrx47wpg0quv",
+                "name": "radio audio",
+                "author": "106.3 MHZ kathmandu",
+                "cover": "img/radioaudio.png"
+            },
+            {
+                "src": "https://live.itech.host:9598/stream",
+                "name": "shreenagar fm",
+                "author": "93.2 MHZ tansen palpa",
+                "cover": "img/shreenagarfm.png"
+            },
+            {
+                "src": "https://live.itech.host:8379/stream",
+                "name": "Radio Bhorukawa",
+                "author": "bhorukawa",
+                "cover": "img/radio-bhorukawa.png"
+            },
+            {
+                "src": "https://usa15.fastcast4u.com/proxy/hitsfm912?mp=/1",
+                "name": "Hits FM",
+                "author": "hits",
+                "cover": "img/hits_fm.png"
+            },
+            {
+                "src": "https://shoutcast.prixa.live/annapurna",
+                "name": "Radio Annapurna Nepal",
+                "author": "annapurnanepal",
+                "cover": "img/radio_annapurna_nepal.png"
+            },
+            {
+                "src": "https://listen-ssvcbfbs.sharp-stream.com/ssvcbfbs3.aac",
+                "name": "BFBS Gurkha Radio",
+                "author": "bfbsgurkha",
+                "cover": "img/bfbs_gurkha_radio.png"
+            },
+            {
+                "src": "https://stream.zeno.fm/60tx8fw9dd0uv",
+                "name": "Radio Tufan",
+                "author": "tufan",
+                "cover": "img/radio_tufan.png"
+            },
+            {
+                "src": "https://streaming.softnep.net:10982/;stream.nsv&type:mp3&volume:10",
+                "name": "Capital FM",
+                "author": "capital",
+                "cover": "img/capital_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:8927/stream",
+                "name": "Radio Amargadhi",
+                "author": "amargadhi",
+                "cover": "img/radio_amargadhi.png"
+            },
+            {
+                "src": "https://stream.zeno.fm/gubb557z4k8uv",
+                "name": "Barahathawa fm 101.1Mhz",
+                "author": "barahathawa1011",
+                "cover": "img/barahathawa_fm.png"
+            },
+            {
+                "src": "https://onlineradio.websoftitnepal.com/8002/stream",
+                "name": "Sky FM 106.6",
+                "author": "sky",
+                "cover": "img/sky_fm_106_6.png"
+            },
+            {
+                "src": "https://streaming.webhostnepal.com/8010/stream",
+                "name": "Shuklaphanta FM",
+                "author": "shuklaphanta",
+                "cover": "img/shuklaphanta_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:8167/stream?1611505122592",
+                "name": "Good News FM",
+                "author": "goodnewsfm",
+                "cover": "img/good_news_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:3260/stream",
+                "name": "Radio Resunga",
+                "author": "resunga",
+                "cover": "img/radio_resunga.png"
+            },
+            {
+                "src": "https://streaming.softnep.net:10874/;stream.nsv&type:mp3&volume:50",
+                "name": "Radio Rudraksha",
+                "author": "rudraksha",
+                "cover": "img/radio_rudraksha.png"
+            },
+            {
+                "src": "https://stream.hamropatro.com/8783",
+                "name": "Classic FM",
+                "author": "classic",
+                "cover": "img/classic_fm.png"
+            },
+            {
+                "src": "https://astream.nepalipatro.com.np:8119/index.html",
+                "name": "Times FM",
+                "author": "times",
+                "cover": "img/times_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:8749/stream",
+                "name": "Pathibhara FM",
+                "author": "pathibhara",
+                "cover": "img/pathibhara_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:8434/stream",
+                "name": "Krishnasar FM",
+                "author": "krishnasar",
+                "cover": "img/krishnasar_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:3880/stream",
+                "name": "Synergy FM",
+                "author": "synergy",
+                "cover": "img/synergy_fm.png"
+            },
+            {
+                "src": "https://live.itech.host:8939/stream",
+                "name": "Radio Prakriti",
+                "author": "prakriti",
+                "cover": "img/radio_prakriti.png"
+            },
+            {
+                "src": "https://live.itech.host:8681/stream",
+                "name": "Radio Nepalbani",
+                "author": "nepalbani",
+                "cover": "img/radio_nepalbani.png"
+            },
+            {
+                "src": "https://streaming.webhostnepal.com:7032/;stream.nsv&type:mp3&volume:30",
+                "name": "Radio Marsyangdi",
+                "author": "marsyangdi",
+                "cover": "img/radio_marsyangdi.png"
+            },
+            
+        ]
+    });
+}
